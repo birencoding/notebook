8539b14027710bce9ec470b3154cbe93a1a56b0e
bond <birendra.developer@gmail.com>
2025-07-14 09:14:42 -0700

    fit css

--- notebook/settings.py
+++ notebook/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%$w3v3rsv$(ij-2$5nrmamtif+ris!u-to^xj&4^yd!veykd-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
