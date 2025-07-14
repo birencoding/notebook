cfd9222a067ea6624b108801a47196d5dc0d4a5a
bond <birendra.developer@gmail.com>
2025-07-14 03:25:04 -0700

    fix css

--- build.sh
+++ build.sh
@@ -1,11 +1,40 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "**********Building NoteBook Project*********"
-python3.12 -m pip install -r requirements.txt
+# echo "**********Building NoteBook Project*********"
+# python3.12 -m pip install -r requirements.txt
 
-echo "**********Make Migrations***************"
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+# echo "**********Make Migrations***************"
+# python3.12 manage.py makemigrations --noinput
+# python3.12 manage.py migrate --noinput
 
-echo "******Collect Static*********"
-python3.12 manage.py collectstatic --noinput --clear
+# echo "******Collect Static*********"
+# python3.12 manage.py collectstatic --noinput --clear
+# {
+#   "builds": [
+#     {
+#       "src": "notebook/wsgi.py",
+#       "use": "@vercel/python",
+#       "config": {
+#         "maxLambdaSize": "15mb",
+#         "runtime": "python3.12"
+#       }
+#     },
+#     // {
+#     //     "src":"build_files.sh",
+#     //     "use":"@vercel/static-build",
+#     //     "config":{
+#     //         "distDir":"staticfiles"
+#     //     }
+#     // }
+#   ],
+#   "routes": [
+#     {
+#       "src": "/static/(.*)",
+#       "dest": "/static/$1"
+#     },
+#     {
+#       "src": "/(.*)",
+#       "dest": "notebook/wsgi.py"
+#     }
+#   ]
+# }
