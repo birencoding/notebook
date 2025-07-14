bcffb1fc680756f2e17de416b08d213e70a57fe5
bond <birendra.developer@gmail.com>
2025-07-14 02:58:53 -0700

    fixing staticfiles

--- notebook/urls.py
+++ notebook/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('user/', include('users.urls')),   
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
